6551091da2e79b78f3693050c7b0aca3512326b5
MelissaLinanGonzalez <melissa16800@gmail.com>
2025-10-14 13:26:44 +0200

    Finalización de ejercicio 2 de la relación2 de ejercicios de javaScript

---
+++ Trabajando_JavaScript2/Ejercicio2/script.js
@@ -0,0 +1,9 @@
+function enfoque(cuadro){
+    cuadro.style.backgroundColor = "lightgray";
+    cuadro.style.color = "blue";
+}
+
+function desenfoque(cuadro){
+    cuadro.style.backgroundColor = "white";
+    cuadro.style.color = "black";
+}
