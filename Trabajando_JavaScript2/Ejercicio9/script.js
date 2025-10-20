dac5d938fcc296dd0e109060f470c30dae2577a1
MelissaLinanGonzalez <melissa16800@gmail.com>
2025-10-20 18:23:29 +0200

    Ejercicio 9 de javaScript

---
+++ Trabajando_JavaScript2/Ejercicio9/script.js
@@ -0,0 +1,16 @@
+function comprobarMostrar(){
+    const entradas = document.querySelectorAll('input[type="text"]')
+
+    entradas.forEach(element => {
+        const valor = element.value.trim();
+        const tieneDigito = /\d/.test(valor);
+
+        if (valor === '' || tieneDigito) {
+            element.classList.add('inputError');
+            
+        } else {
+            element.classList.remove('inputError');
+        }
+    })
+
+}
