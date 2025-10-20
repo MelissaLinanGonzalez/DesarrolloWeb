0ff25b42267f48506c31c4aa25d075ef61185806
MelissaLinanGonzalez <melissa16800@gmail.com>
2025-10-20 12:46:53 +0200

    Ejercicio 7 de javaScript

---
+++ Trabajando_JavaScript2/Ejercicio7/script.js
@@ -0,0 +1,23 @@
+function crearTablaNumerosPares(){
+    const numero = parseInt(document.getElementById('numero').value);
+    const contenedor = document.getElementById('contenedor');
+
+    contenedor.innerHTML = "";
+
+    const tabla = document.createElement('table');
+    tabla.border = "1px solid black";
+    tabla.style.borderCollapse = "collapse";
+
+    for (let i = 0; i < numero; i++) {
+        if (i % 2 === 0){
+            const fila = document.createElement('tr');
+            const celda = document.createElement('td');
+
+            celda.textContent = i;
+            fila.appendChild(celda);
+            tabla.appendChild(fila);
+        }
+        contenedor.appendChild(tabla);
+
+    }
+}
