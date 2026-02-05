60cb85fcfc80e07925c2f222473e270c4f9e97c4
MelissaLinanGonzalez <melissa16800@gmail.com>
2026-02-05 20:27:01 +0100

    Modificado ejercicio para su realización con promesas

--- CallBacks, DOM y promesas/Opcionales/Ej2.js
+++ CallBacks, DOM y promesas/Opcionales/Ej2.js
@@ -3,11 +3,33 @@
 
 let cadenaOperaciones = "1 + 3 * 4 - (6 / 2)"; 
 
-function calcular(cadenaOperaciones){
-    //return eval(cadenaOperaciones); // PELIGROSO.
+// function calcular(cadenaOperaciones){
+//     //return eval(cadenaOperaciones); // PELIGROSO.
 
-    // alternativa al anterior, crea una funcion temporal que devuelve el resultado de la cadena.
-    return new Function('return ' + cadenaOperaciones)(); 
+//     // alternativa al anterior, crea una funcion temporal que devuelve el resultado de la cadena.
+//     return new Function('return ' + cadenaOperaciones)(); 
+// }
+
+// console.log(calcular(cadenaOperaciones))
+
+function calcularConPromesa(cadena){
+    let promesa = new Promise((resolver, rechazar) => {
+
+        try{
+            let resultado = new Function('return ' + cadena)();
+            if (isNaN(resultado)){
+                rechazar("Error: El resultado no es un número válido");
+            } else{
+                resolver(resultado);
+            }
+        } catch(error){
+            rechazar("Error en la sintaxis de la operación: " + error.message);
+        }
+        
+    });
+    return promesa;
 }
 
-console.log(calcular(cadenaOperaciones))
+calcularConPromesa(cadenaOperaciones)
+    .then(resultado => console.log("Resultado:", resultado))
+    .catch(error => console.error(error));
